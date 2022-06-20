1d523c4cdce50a0fdf8ec51ed095d30e91599da6
Dixit Hihoriya <hihoriyadixit1@gmail.com>
2022-06-19 21:11:54 -0400

    Updated portfolio

--- config/db.js
+++ config/db.js
@@ -3,6 +3,7 @@
 <!--     StudentID: 301201312     -->
 <!--     Date : 19th June 2022     -->*/
 
+// Database URI
 module.exports = {
     "URI" : "mongodb+srv://dhihoriya:xbhl2cfJWJKWMhNu@cluster0.pvcvjxu.mongodb.net/test?retryWrites=true&w=majority",
     "Secret": 'SomeSecret'
